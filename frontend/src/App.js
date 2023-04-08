3f8482ab1df019282e7368046a8d02dfad95ee49
Pham Anh <contact.vananh0505@gmail.com>
2023-04-08 14:22:30 +0700

    final rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react'
 
+import { Container } from 'react-bootstrap'
 
-const  App =()=> {
+import Header from '../src/components/Header'
+import Footer from '../src/components/Footer'
+import HomeScreen from './screens/HomeScreen'
+const App = () => {
   return (
     <>
-    <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
